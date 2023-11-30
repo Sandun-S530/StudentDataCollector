19f9566502cd0ca5f34725ae4af2176448c7e251
Sandun Shamika <sandunofficial20@gmail.com>
2023-11-30 18:24:29 +0000

    Login activity updated

--- app/src/main/java/com/example/nibm/Login.java
+++ app/src/main/java/com/example/nibm/Login.java
@@ -7,21 +7,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
+    EditText username_input, password_input;
+    String username, password;
     AppCompatButton btn_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        username_input = findViewById(R.id.username);
+        password_input = findViewById(R.id.password);
         btn_login = findViewById(R.id.btn_login);
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Login.this, AdminPanel.class);
-                startActivity(intent);
+                username = username_input.getText().toString().trim();
+                password = password_input.getText().toString().trim();
+
+                if("admin".equals(username) && "1234".equals(password)){
+                    Intent intent = new Intent(Login.this, AdminPanel.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(Login.this, "Invalid Username or Password", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
